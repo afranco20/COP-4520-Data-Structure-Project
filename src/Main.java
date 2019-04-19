0bbb43d13942d4b8a4aebe46d56947d54981c903
Alex Franco <26696700+afranco20@users.noreply.github.com>
2019-04-19 16:44:49 -0400

    update test code

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,53 @@
 import java.util.ArrayDeque;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 import java.util.stream.IntStream;
 
 public class Main {
-  private static final int num_threads = 8;
-
-  // generate list of numbers with same hash
-  private static ArrayDeque<Integer> hashCollider() {
-    ArrayDeque<Integer> hashes;
-
-    // filters infinite list of integers and collects the first 100 results
-    hashes = IntStream.iterate(0, i -> i + 1)
-        .parallel()
-        .filter(i -> i % 16 == 6)
-        .limit(100000)
-        .boxed()
-        .collect(Collectors.toCollection(ArrayDeque::new));
-
-    // return list
-    return hashes;
-  }
-
   // generate list of sequential numbers
-  private static ArrayDeque<Integer> numbers() {
-    return IntStream.rangeClosed(0, 1_000_000).boxed().collect(Collectors.toCollection(ArrayDeque::new));
+  private static ArrayDeque<Integer> generate() {
+    ArrayDeque<Integer> list = new ArrayDeque<>();
+
+    IntStream.iterate(0, i -> i + 1)
+        .limit(1_000_000)
+        .forEach(i -> list.add(ThreadLocalRandom.current().nextInt()));
+
+    return list;
   }
 
   public static void main(String[] args) {
-    CacheTrie test = new CacheTrie();
+    CacheTrie cache_trie = new CacheTrie();
 
-    System.out.println("--- test insertions ---");
-    ArrayDeque<Integer> hashes = hashCollider();
-    //ArrayDeque<Integer> hashes = numbers();
+    System.out.println("--- test generate ---");
+    ArrayDeque<Integer> list = generate();
 
-    /*ExecutorService thread_pool = Executors.newFixedThreadPool(num_threads);
+    ExecutorService thread_pool;
+    long startTime;
+    long endTime;
 
-    for (int i = 0; i < 1_000_000; i++) {
-      final int k = i;
-      thread_pool.submit(() -> test.insert(k, k));
+    for (int num_threads = 1; num_threads <= 32; num_threads <<= 1) {
+      thread_pool = Executors.newFixedThreadPool(num_threads);
+      List<Callable<Object>> tasks = new ArrayList<>();
+
+      list.forEach(i -> {
+        tasks.add(() -> {
+          cache_trie.fastInsert(i, i);
+          return null;
+        });
+      });
+
+      startTime = System.nanoTime();
+      try {
+        thread_pool.invokeAll(tasks);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+
+      thread_pool.shutdown();
+      endTime = System.nanoTime();
+
+      System.out.printf("%d, %f%n", num_threads, ((double) (endTime - startTime) / 1E6));
     }
-
-    thread_pool.shutdown();
-    System.out.println("=== test ===");*/
-
-    for (Integer i : hashes) {
-      // System.out.printf("%n*** INSERTING %d ***%n%n", i);
-//      test.insert(i, i);
-      test.fastInsert(i, i);
-    }
-
-    System.out.println("--- test lookup ---");
-    for (Integer i : hashes) {
-      //System.out.println(test.fastLookup(i));
-//      test.lookup(i);
-      test.fastLookup(i);
-    }
-
-    //System.out.println("--- test trace ---");
-    //test.printTrace();
-
-    System.out.println("--- test cache ---");
-    //test.printCache();
   }
 }
